6c3f091d4635e3eff279d15d280389352215ba52
giorgi <giorgimegrelishvili45@gmail.com>
2020-10-12 21:10:25 +0400

    test_commit

--- assignmet#1/routes/web.php
+++ assignmet#1/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/posts',[\App\Http\Controllers\PostControler::class, 'index']);
+Route::get('posts/create', [\App\Http\Controllers\PostControler::class, 'create'])->name('post.create');
+Route::get('/posts/{id}', [\App\Http\Controllers\PostControler::class, 'show']);
+Route::post('posts/savepost', [\App\Http\Controllers\PostControler::class, 'save'])->name('post.save');
+Route::get('posts/{id}/edit', [\App\Http\Controllers\PostControler::class, 'edit'])->name('post.edit');
+Route::put('posts/{id}/update', [\App\Http\Controllers\PostControler::class, 'update'])->name('post.update');
+Route::delete('posts/{id}/delete', [\App\Http\Controllers\PostControler::class, 'delete'])->name('post.delete');
